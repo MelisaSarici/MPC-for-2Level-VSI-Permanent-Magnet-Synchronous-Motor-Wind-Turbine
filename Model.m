88b15e2eeb972d127fa77ea9fb92f30921c1510b
MelisaSarici <sarici.asena@metu.edu.tr>
2018-06-05 22:14:35 +0300

    Simulation file is extented
    Although I wanted to use the MPC code for the simulink for loop, dare function is not suitable for standalone operation.Hence, I will try to buildthe system in mfile.

---
+++ Model.m
@@ -0,0 +1,10 @@
+%% Model Parameters
+set(groot,'defaulttextinterpreter','latex');  
+set(groot, 'defaultAxesTickLabelInterpreter','latex');  
+set(groot, 'defaultLegendInterpreter','latex'); 
+clear all;
+
+
+open_system('electrical_model.slx'); 
+
+
